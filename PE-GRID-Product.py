47d991e796a0aeffe25456a7506c5d62cfff1cd1
YesseGriffen <146498196+YesseGriffen@users.noreply.github.com>
2023-09-29 20:40:25 -0500

    PE-GRID-Product.py

--- PE-GRID-Product.py
+++ PE-GRID-Product.py
@@ -1,5 +1,7 @@
 
-
+#This program will find a the largest product in the grid of numbers that are next to eachother forming a line...
+#The line can be diagonal or straight...
+#The program default is set to 4, meaning it will find the largest product of 4 numbers that are in a line.
 grid_str = """
 08 02 22 97 38 15 00 40 00 75 04 05 07 78 52 12 50 77 91 08 
 49 49 99 40 17 81 18 57 60 87 17 40 98 43 69 48 04 56 62 00

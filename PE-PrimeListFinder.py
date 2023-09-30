b0846b903251d93e1156899332ac2d39b96e0c57
YesseGriffen <146498196+YesseGriffen@users.noreply.github.com>
2023-09-29 20:43:06 -0500

    Add files via upload

---
+++ PE-PrimeListFinder.py
@@ -0,0 +1,25 @@
+#This program upon entering a number will represent the prime that comes in counting order.
+#For example entering 2, gives you 3. As 3 is the second prime in counting order.
+
+def prime(n):
+    if n < 2:
+        return False
+    for i in range(2, int(n ** 0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
+
+
+def find_prime(n):
+    count = 0
+    num = 2  
+    while count < n:
+        if prime(num):
+            count += 1
+        if count == n:
+            return num
+        num += 1
+
+
+n = int(input())
+print(find_prime(n))
